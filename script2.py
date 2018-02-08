1a7638767ebb7b2da88c4bb56c486c5f5702b168
pawenowa@yahoo.com <pawenowa@yahoo.com>
2018-02-08 20:53:44 +0100

    Hasło Moria

---
+++ script2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+print("Stoisz przed kamienną bramą podczas pełni księżyca. ")
+password = input("Podaj hasło: ")
+if password.lower() == "melon":
+    print("Możesz wejść, brama została otwarta!")
+else:
+    print("Hasło nie zadziałało")
